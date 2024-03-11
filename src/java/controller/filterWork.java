0e668f45dd9fd793eec9989cdc7e450dfcc84f49
Duong Van Hoan <121860612+Harim1010@users.noreply.github.com>
2024-03-11 21:54:42 +0700

    Update filterWork.java

--- src/java/controller/filterWork.java
+++ src/java/controller/filterWork.java
@@ -1,92 +1,92 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-package controller;
+// /*
+//  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+//  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+//  */
+// package controller;
 
-import DAO.AccountDAO;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import model.trans_history;
+// import DAO.AccountDAO;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServlet;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import java.io.IOException;
+// import java.io.PrintWriter;
+// import java.util.ArrayList;
+// import java.util.List;
+// import model.trans_history;
 
-/**
- *
- * @author caoth
- */
-public class filterWork extends HttpServlet {
+// /**
+//  *
+//  * @author caoth
+//  */
+// public class filterWork extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            String work = request.getParameter("work");
-            AccountDAO dao = new AccountDAO();
-            List<trans_history> listTransHis;
+//     /**
+//      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+//      * methods.
+//      *
+//      * @param request servlet request
+//      * @param response servlet response
+//      * @throws ServletException if a servlet-specific error occurs
+//      * @throws IOException if an I/O error occurs
+//      */
+//     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//             throws ServletException, IOException {
+//         response.setContentType("text/html;charset=UTF-8");
+//         try ( PrintWriter out = response.getWriter()) {
+//             /* TODO output your page here. You may use following sample code. */
+//             String work = request.getParameter("work");
+//             AccountDAO dao = new AccountDAO();
+//             List<trans_history> listTransHis;
 
-// Kiểm tra xem người dùng đã chọn loại giao dịch là gì
-            if ("processed".equals(work)) {
-                listTransHis = dao.getTransByWork(true); // Lấy các giao dịch Positive
-            } else {
-                listTransHis = dao.getTransByWork(false); // Lấy các giao dịch Negative
-            }
-            request.setAttribute("listA", listTransHis);
-            request.getRequestDispatcher("TransHistory.jsp").forward(request, response);
-        }
-    }
+// // Kiểm tra xem người dùng đã chọn loại giao dịch là gì
+//             if ("processed".equals(work)) {
+//                 listTransHis = dao.getTransByWork(true); // Lấy các giao dịch Positive
+//             } else {
+//                 listTransHis = dao.getTransByWork(false); // Lấy các giao dịch Negative
+//             }
+//             request.setAttribute("listA", listTransHis);
+//             request.getRequestDispatcher("TransHistory.jsp").forward(request, response);
+//         }
+//     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+//     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+//     /**
+//      * Handles the HTTP <code>GET</code> method.
+//      *
+//      * @param request servlet request
+//      * @param response servlet response
+//      * @throws ServletException if a servlet-specific error occurs
+//      * @throws IOException if an I/O error occurs
+//      */
+//     @Override
+//     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//             throws ServletException, IOException {
+//         processRequest(request, response);
+//     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+//     /**
+//      * Handles the HTTP <code>POST</code> method.
+//      *
+//      * @param request servlet request
+//      * @param response servlet response
+//      * @throws ServletException if a servlet-specific error occurs
+//      * @throws IOException if an I/O error occurs
+//      */
+//     @Override
+//     protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//             throws ServletException, IOException {
+//         processRequest(request, response);
+//     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+//     /**
+//      * Returns a short description of the servlet.
+//      *
+//      * @return a String containing servlet description
+//      */
+//     @Override
+//     public String getServletInfo() {
+//         return "Short description";
+//     }// </editor-fold>
 
-}
+// }
